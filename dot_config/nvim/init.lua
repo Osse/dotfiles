ca8c7b3f773da6f07ee69b3e8cc276601b215bce
Øystein Walle <oystwa@gmail.com>
2024-10-14 09:18:31 +0200

    nvim: WIP cmp

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -157,6 +157,54 @@ require("lazy").setup({
             end
         },
         {
+            "hrsh7th/nvim-cmp",
+            -- load cmp on InsertEnter
+            event = "InsertEnter",
+            -- these dependencies will only be loaded when cmp loads
+            -- dependencies are always lazy-loaded unless specified otherwise
+            dependencies = {
+                'neovim/nvim-lspconfig',
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-buffer",
+                "hrsh7th/cmp-path",
+            },
+            config = function()
+                local cmp = require'cmp'
+                cmp.setup({
+                    snippet = {
+                        -- REQUIRED by nvim-cmp. get rid of it once we can
+                        expand = function(args)
+                            vim.fn["vsnip#anonymous"](args.body)
+                        end,
+                    },
+                    mapping = cmp.mapping.preset.insert({
+                        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                        ['<C-Space>'] = cmp.mapping.complete(),
+                        ['<C-e>'] = cmp.mapping.abort(),
+                        -- Accept currently selected item.
+                        -- Set `select` to `false` to only confirm explicitly selected items.
+                        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    }),
+                    sources = cmp.config.sources({
+                        { name = 'nvim_lsp' },
+                    }, {
+                        { name = 'path' },
+                    }),
+                    experimental = {
+                        ghost_text = true,
+                    },
+                })
+
+                -- Enable completing paths in :
+                cmp.setup.cmdline(':', {
+                    sources = cmp.config.sources({
+                        { name = 'path' }
+                    })
+                })
+            end
+        },
+        {
             'Osse/cmake-tools.nvim',
             dependencies = { 'nvim-lua/plenary.nvim' },
             opts = {
