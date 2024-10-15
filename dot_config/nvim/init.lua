b6087b4f08fa13b6298397ae2a3b390d943c8051
Øystein Walle <oystwa@gmail.com>
2024-10-15 15:59:29 +0200

    wip

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -205,11 +205,23 @@ require("lazy").setup({
             end
         },
         {
-            'Osse/cmake-tools.nvim',
-            branch = "all-fixes",
-            dependencies = { 'nvim-lua/plenary.nvim' },
-            opts = {
-                cmake_generate_options = {},
+            dir = '~/dev/cmake-tools.nvim'
+            -- 'Osse/cmake-tools.nvim',
+            -- branch = "all-fixes",
+            -- dependencies = { 'nvim-lua/plenary.nvim' },
+            -- opts = {
+            --     cmake_generate_options = {},
+            -- }
+        },
+        {
+            'nvim-telescope/telescope.nvim',
+            tag = '0.1.8',
+            dependencies = {
+                'nvim-lua/plenary.nvim',
+                {
+                    'nvim-telescope/telescope-fzf-native.nvim',
+                    build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release'
+                }
             }
         }
     },
