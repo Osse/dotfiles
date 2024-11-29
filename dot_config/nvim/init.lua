ba09c3a377c22ee14298767a7a767bc33e94ed1a
Øystein Walle <oystwa@gmail.com>
2024-11-29 08:44:54 +0100

    fixup! statusline for qml

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -87,7 +87,7 @@ require("lazy").setup({
             'nvim-treesitter/nvim-treesitter',
             build = ':TSUpdate',
             opts = {
-                ensure_installed = { "c", "cmake", "cpp", "just", "lua", "markdown", "python", "rust", "vim", "vimdoc", "toml", "yaml" },
+                ensure_installed = { "c", "cmake", "cpp", "just", "lua", "markdown", "python", "qmljs", "qmldir", "rust", "vim", "vimdoc", "toml", "yaml" },
                 sync_install = false,
                 highlight = { enable = true },
                 indent = { enable = true },
