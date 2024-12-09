79e3821a3fa8dfac9b0b51a14747a36d5976e8a5
Øystein Walle <oystwa@gmail.com>
2024-12-09 09:36:18 +0100

    nvim: Tidy lualine

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,18 +1,19 @@
-local function code_context()
-    local opts = {}
-
-    if vim.bo.filetype == "yaml" then
-        opts.type_patterns = { "block_mapping_pair" }
-        opts.transform_fn = function(text, node)
+local ft_opts = {
+    yaml = {
+        type_patterns = { "block_mapping_pair" },
+        transform_fn = function(text, node)
             -- Strip ":" from parent nodes and ": some value" or ": |" from the current node
             local i = string.find(text, ":") or 0
             return string.sub(text, 1, i-1)
         end
-    elseif vim.bo.filetype == "qml" then
-        opts.type_patterns = { "ui_object_definition" }
-    end
-    -- else the defaults are good enough for now
+    },
+    qml = {
+        type_patterns = { "ui_object_definition" }
+    },
+}
 
+local function code_context()
+    local opts = ft_opts[vim.bo.filetype] or {} -- else the defaults are good enough for now
     return require("nvim-treesitter").statusline(opts)
 end
 
@@ -22,7 +23,7 @@ return {
         options = {
             theme = 'jellybeans'
         },
-        extensions = { 'quickfix' },
+        extensions = { 'overseer', 'quickfix' },
         sections = {
             lualine_c = {
                 {
