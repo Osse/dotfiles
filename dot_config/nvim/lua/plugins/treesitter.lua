28f3063319347d6d6420ac4205e4cb8492da8f8b
Øystein Walle <oystwa@gmail.com>
2024-12-12 14:51:05 +0100

    nvim: Fix treesitter config

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    main = 'nvim-treesitter.configs',
     opts = {
         ensure_installed = { "c", "cmake", "cpp", "just", "lua", "markdown", "python", "qmljs", "qmldir", "rust", "vim", "vimdoc", "toml", "yaml" },
         sync_install = false,
