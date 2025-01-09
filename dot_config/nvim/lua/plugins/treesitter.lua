2b119c5aed81ed78ed1997295b015d1e921f0734
Øystein Walle <oystwa@gmail.com>
2025-01-09 20:04:26 +0100

    nvim: Disable treesitter indent for cmake
    Buggy behavior with (

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -6,6 +6,9 @@ return {
         ensure_installed = { "c", "cmake", "cpp", "just", "lua", "markdown", "python", "qmljs", "qmldir", "rust", "vim", "vimdoc", "toml", "yaml" },
         sync_install = false,
         highlight = { enable = true },
-        indent = { enable = true },
+        indent = {
+            enable = true,
+            disable = { "cmake" }
+        },
     }
 }
