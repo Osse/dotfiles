84b81d127af800b240afb02aa203d7c3bfef4af1
Øystein Walle <oystwa@gmail.com>
2025-02-02 07:43:31 +0100

    qml: improve

--- dot_config/nvim/after/ftplugin/qml.lua
+++ dot_config/nvim/after/ftplugin/qml.lua
@@ -1,16 +1,16 @@
 local Path = require'plenary.path'
 
 vim.opt.suffixesadd:append(".qml")
-local buf_path = Path:new(vim.api.nvim_buf_get_name(0)):parent():parent()
+local buf_path = Path:new(vim.api.nvim_buf_get_name(0)):parent()
 
 for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 1, 20, false)) do
     local _, _, capture = string.find(line, 'import "([^"]*)"')
     if capture ~= nil then
         local include_path
         if string.find(capture, "../") == 1 then
-            include_path = buf_path / string.sub(capture, 4)
+            include_path = buf_path:parent() / string.sub(capture, 4)
         else
-            include_path = Path:new(capture)
+            include_path = buf_path / Path:new(capture)
         end
 
         if include_path:is_dir() then
@@ -18,3 +18,5 @@ for _, line in ipairs(vim.api.nvim_buf_get_lines(0, 1, 20, false)) do
         end
     end
 end
+
+vim.keymap.set('n', 'gd', 'gf', { buffer = true })
