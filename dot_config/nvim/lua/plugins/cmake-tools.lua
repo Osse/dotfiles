a46c593b325315689e91a8006dcf13cbac13fd60
Øystein Walle <oystwa@gmail.com>
2026-01-28 15:03:08 +0100

    nvim: overseer/cmake changes

--- dot_config/nvim/lua/plugins/cmake-tools.lua
+++ dot_config/nvim/lua/plugins/cmake-tools.lua
@@ -1,19 +1,19 @@
 local function on_new_task(task)
     local o = require("overseer")
 
-    task:subscribe("on_complete", function(_, result)
-        if result == "SUCCESS" then
+    task:subscribe("on_complete", function(t, result)
+        if result == "SUCCESS" or result == "CANCELED" then
             o.close()
             vim.cmd("cclose")
         end
     end)
 
+    vim.cmd("cclose")
     o.open( { enter = false, focus_task_id = task.id })
 end
 
 return {
-    'Osse/cmake-tools.nvim',
-    branch = "all-fixes",
+    'Civitasv/cmake-tools.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = {
         cmake_generate_options = {},
@@ -23,11 +23,20 @@ return {
             opts = {
                 on_new_task = function(task)
                     local cm = require('cmake-tools')
-                    task.name = "Build " .. (cm.get_build_target() or "default")
+                    if task.name:find("--build") then
+                        local t = cm.get_build_target()
+                        if t and t[1] then
+                            task.name = "Build " .. t[1]
+                        else
+                            task.name = "Build default"
+                        end
+                    end
                     on_new_task(task)
                 end,
                 new_task_opts = {
-                    components = { { "on_output_quickfix", open = false, open_on_exit = "failure" }, "default" },
+                    strategy = {
+                        "jobstart"
+                    },
                 }
             },
         },
@@ -36,9 +45,15 @@ return {
             opts = {
                 on_new_task = function(task)
                     local cm = require('cmake-tools')
-                    task.name = "Run " .. (cm.get_build_target() or "default") .. " " .. table.concat(cm.get_launch_args(), " ")
+                    task.name = "Run " .. (cm.get_build_target()[1] or "default") .. " " .. table.concat(cm.get_launch_args(), " ")
+                    task.cwd = vim.fn.getcwd()
                     on_new_task(task)
-                end
+                end,
+                new_task_opts = {
+                    strategy = {
+                        "jobstart"
+                    },
+                }
             },
         },
         cmake_virtual_text_support = false,
@@ -51,6 +66,7 @@ return {
                 local cm = require('cmake-tools')
                 vim.keymap.set('n', '<F5>', function() cm.build({}) end)
                 vim.keymap.set('n', '<F6>', function() cm.run({}) end)
+                vim.keymap.set('n', '<F7>', function() cm.build_current_file({}) end)
                 vim.keymap.set('n', '<F18>', function() cm.stop_executor({}) end)
                 vim.keymap.set('n', '<S-F6>', function() cm.stop_executor({}) end)
             end
