385da8c589e2b5f679eb63401225210bc556712c
Øystein Walle <oystwa@gmail.com>
2026-01-28 15:02:14 +0100

    nvim: main treesitter

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,8 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs',
+    branch = 'main',
     opts = {
-        ensure_installed = { "c", "cmake", "cpp", "just", "lua", "markdown", "python", "qmljs", "qmldir", "rust", "vim", "vimdoc", "toml", "yaml" },
-        sync_install = false,
         highlight = { enable = true },
         indent = {
             enable = true,
