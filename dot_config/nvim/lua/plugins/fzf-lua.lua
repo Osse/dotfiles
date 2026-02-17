f2b299fd90fc16d4b7a2adbfd8494c5810fabda0
Øystein Walle <oystwa@gmail.com>
2026-02-17 12:14:42 +0100

    nvim: fzf updates

--- dot_config/nvim/lua/plugins/fzf-lua.lua
+++ dot_config/nvim/lua/plugins/fzf-lua.lua
@@ -3,13 +3,12 @@ local is_inside_work_tree = {}
 local function project_files(opts)
   opts = opts or {} -- define here if you want to define something
 
-  local cwd = vim.fn.getcwd()
-  if is_inside_work_tree[cwd] == nil then
+  if is_inside_work_tree[opts.cwd] == nil then
     vim.fn.system("git rev-parse --is-inside-work-tree")
-    is_inside_work_tree[cwd] = vim.v.shell_error == 0
+    is_inside_work_tree[opts.cwd] = vim.v.shell_error == 0
   end
 
-  if is_inside_work_tree[cwd] then
+  if is_inside_work_tree[opts.cwd] then
     require('fzf-lua').git_files(opts)
   else
     require('fzf-lua').files(opts)
@@ -27,10 +26,9 @@ return {
         { "<leader>F", '<cmd>FzfLua live_grep<cr>', desc = "Live grep" },
         { 'Q', '<cmd>FzfLua buffers<CR>', desc = "Switch buffers" },
         { "<Leader>f", '<cmd>FzfLua grep_cword<cr>', desc = "grep string" },
-        { '<C-P>', project_files, desc = "project files" },
-        { '<Leader><C-P>', function() require('fzf-lua').git_files({ fzf_opts = { ["--query"] = vim.fn.expand("<cword>") } }) end, desc = "project files" },
-        -- { '<C-P>', function() project_files({ default_text = vim.fn.expand("<cword>")}) end },
-        -- { '<Leader>.<C-P>', function() tb.find_files({ cwd = vim.fn.expand("%:p:h")}) end },
+        { '<C-P>', function() project_files({ cwd = vim.fn.getcwd() }) end, desc = "project files under current directory" },
+        { '<Leader><C-P>', function() project_files({ cwd = vim.fn.getcwd(), fzf_opts = { ["--query"] = vim.fn.expand("<cword>") } }) end, desc = "project files containing word under current directory" },
+        { '<Leader>/<C-P>', function() require('fzf-lua').git_files() end, desc = "project files from root" },
     },
     init = function()
 	require('fzf-lua').register_ui_select()
