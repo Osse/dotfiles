680fda44d7ec976bd8587f6b56ac1e2ee6604f68
Øystein Walle <oystwa@gmail.com>
2026-05-19 12:45:59 +0200

    nvim: Remove unused kitty plugin

--- dot_config/nvim/lua/plugins/kitty-scrollback.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'mikesmithgh/kitty-scrollback.nvim',
-    enabled = true,
-    lazy = true,
-    cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
-    event = { 'User KittyScrollbackLaunch' },
-    opts = {}
-}
