578fd7d0d9a01f5f80925cacc1d355e593407dfd
Øystein Walle <oystwa@gmail.com>
2026-05-22 09:36:54 +0200

    lualine: try out cmake stuff

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -29,6 +29,28 @@ local function is_not_debugging()
     return not is_debugging()
 end
 
+local cmake = require("cmake-tools")
+
+local function run(cmd)
+    return function(n, mouse)
+       if n == 1 and mouse == "l" then
+           -- Feed through the input queue so it runs *after* the pending mouse
+           -- release; otherwise fzf-lua's `startinsert` gets clobbered and the
+           -- picker opens in normal mode.
+           vim.api.nvim_feedkeys(":" .. cmd .. "\r", "n", false)
+       end
+   end
+end
+
+local function combine(s1, s2)
+    local s = (s1 and s1[1] or "X")
+    if s2 and s2 ~= s1[1] then
+        s = s .. "/" .. s2
+    end
+    return s
+end
+
+local icons = require('icons')
 
 return {
     'nvim-lualine/lualine.nvim',
@@ -42,7 +64,60 @@ return {
                 { 'branch' },
                 { 'diff', cond = is_not_debugging },
                 { 'diagnostics', cond = is_not_debugging },
-                { dap_status, cond = is_debugging }
+                { dap_status, cond = is_debugging },
+            },
+            lualine_x = {
+                {
+                  function()
+                    return vim.fs.relpath(vim.fn.getcwd(),cmake.get_config().cwd)
+                  end,
+                  icon = require'nvim-web-devicons'.get_icon_by_filetype("cmake"),
+                  separator = '',
+                  cond = cmake.is_cmake_project,
+                  on_click = run("CMakeSelectConfigurePreset")
+                },
+                {
+                  function()
+                    local c_preset = cmake.get_configure_preset()
+                    local b_preset = cmake.get_build_preset()
+                    return combine({c_preset}, b_preset)
+                  end,
+                  icon = "",
+                  separator = '',
+                  cond = function()
+                    return cmake.is_cmake_project() and cmake.has_cmake_preset()
+                  end,
+                  on_click = run("CMakeSelectConfigurePreset")
+                },
+                {
+                  function()
+                    local b_target = cmake.get_build_target()
+                    local l_target = cmake.get_launch_target()
+                    return combine(b_target, l_target)
+                  end,
+                  separator = '',
+                  icon = "󰓾",
+                  cond = cmake.is_cmake_project,
+                  on_click = run("CMakeSelectBuildTarget")
+                },
+                {
+                  function() return icons.ui.Gear end,
+                  separator = '',
+                  cond = cmake.is_cmake_project,
+                  on_click = run("CMakeBuild")
+                },
+                {
+                  function() return icons.ui.Run end,
+                  separator = '',
+                  cond = cmake.is_cmake_project,
+                  on_click = run("CMakeRun")
+                },
+                {
+                  function() return icons.ui.Debug .. " " end,
+                  separator = '',
+                  cond = cmake.is_cmake_project,
+                  on_click = run("CMakeDebug")
+                },
             },
             lualine_c = {
                 {
@@ -50,7 +125,6 @@ return {
                     path = 1
                 }
             },
-            lualine_x = { code_context }
         },
         inactive_sections = {
             lualine_c = {
